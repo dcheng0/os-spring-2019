67778e6a9615434f7c3295953c930a956fd203cc
dcheng0 <dcheng1@luc.edu>
2019-04-09 20:50:27 -0500

    compiling and passing test.

--- data_structures/queue/bound_buffer.h
+++ data_structures/queue/bound_buffer.h
@@ -48,13 +48,13 @@ public:
         //while buffer is full
         while (size()==max_size_) {
             // empty -> down();
-            int sem_trywait(empty);
+            int sem_trywait(sem_t *empty);
             // mutex -> down();
-            int pthread_mutex_trylock(mutex);
+            int pthread_mutex_trylock(pthread_mutex_t *mutex);
             // int item = produce_item();
-            int item = q.addLast(value);
-            int pthread_mutex_unlock(mutex);
-            int sem_trylock(full);
+            q.addLast(value);
+            int pthread_mutex_unlock(pthread_mutex_t *mutex);
+            int sem_trywait(sem_t *full);
 
 
 
@@ -65,19 +65,20 @@ public:
     ValueType removeFirst(){
 
         while (size()==0){
-            int sem_trylock(full);
+            //int sem_trylock(semt_t *full);
+            int sem_trywait(sem_t *full);
             // mutex locking, waiting, and notification structure from ref 1
             //std::unique_lock<std::mutex> lock(mutex)
-            int pthread_mutex_trylock(mutex);
+            int pthread_mutex_trylock(pthread_mutex_t *mutex);
             // while buffer is empty
-            int item = q.removeFirst();
-            int pthread_mutex_tryunlock(mutex);
+            q.removeFirst();
+            int pthread_mutex_tryunlock(pthread_mutex_t *mutex);
         }
         // remove item
         // decrement size
         size_--;
         // notify threads waiting on full buffer
-        nonfull.notify_all();
+        //nonfull.notify_all();
         return ValueType();
     }
 
